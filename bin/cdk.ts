d000148b280ec1e48b3c6be4e50feb44e35bd0ff
AugustMD <sungjin.park0129@gmail.com>
2025-06-06 18:03:17 +0900

    All-Lay cdk sagemaker, cfn, opensearch 설정

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,10 +2,10 @@
 import "source-map-support/register";
 import { App } from "aws-cdk-lib";
 import { EC2Stack } from "../lib/ec2Stack/ec2Stack";
-// import { OpensearchStack } from "../lib/openSearchStack";
-// import { CustomResourceStack } from "../lib/customResourceStack";
-// import { SagemakerNotebookStack } from "../lib/sagemakerNotebookStack/sagemakerNotebookStack";
-// import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+import { OpensearchStack } from "../lib/openSearchStack";
+import { CustomResourceStack } from "../lib/customResourceStack";
+import { SagemakerNotebookStack } from "../lib/sagemakerNotebookStack/sagemakerNotebookStack";
+import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 const STACK_PREFIX = "AllLay";
 const DEFAULT_REGION = "us-west-2";
@@ -19,16 +19,16 @@ const envSetting = {
 const app = new App();
 
 // Deploy Sagemaker stack
-// const sagemakerNotebookStack = new SagemakerNotebookStack(app, `${STACK_PREFIX}-SagemakerNotebookStack`, envSetting);
+const sagemakerNotebookStack = new SagemakerNotebookStack(app, `${STACK_PREFIX}-SagemakerNotebookStack`, envSetting);
 
 // Deploy OpenSearch stack
-// const opensearchStack = new OpensearchStack(app, `${STACK_PREFIX}-OpensearchStack`, envSetting);
-// opensearchStack.addDependency(sagemakerNotebookStack);
+const opensearchStack = new OpensearchStack(app, `${STACK_PREFIX}-OpensearchStack`, envSetting);
+opensearchStack.addDependency(sagemakerNotebookStack);
 
 // Deploy Reranker stack using cloudformation template
-// const rerankerStack = new CfnInclude(opensearchStack, `${STACK_PREFIX}-RerankerStack`, {
-//   templateFile: 'lib/rerankerStack/RerankerStack.template.json'
-// });
+const rerankerStack = new CfnInclude(opensearchStack, `${STACK_PREFIX}-RerankerStack`, {
+  templateFile: 'lib/rerankerStack/RerankerStack.template.json'
+});
 
 const customResourceStack = new CustomResourceStack(app, `${STACK_PREFIX}-CustomResourceStack`, envSetting)
 customResourceStack.addDependency(opensearchStack);
