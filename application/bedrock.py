b258025f52c6d891634d5349ac43827c8bb509d9
sjpark129 <sungjin.park0129@gmail.com>
2025-06-11 18:12:24 +0900

    버전업

--- application/bedrock.py
+++ application/bedrock.py
@@ -14,7 +14,7 @@ secrets_manager = boto3.client('secretsmanager', region_name=region)
 
 def invoke_agent_direct(query):
     agent_id = os.environ.get("BEDROCK_AGENT_ID", "WZPRJN27KK")
-    alias_id = os.environ.get("BEDROCK_AGENT_ALIAS_ID", "PDSJONTSCI")
+    alias_id = os.environ.get("BEDROCK_AGENT_ALIAS_ID", "CPSTQSLOE9")
     session_id = os.environ.get("SESSION_ID", "default-session")
     region = os.environ.get("AWS_DEFAULT_REGION")
 
